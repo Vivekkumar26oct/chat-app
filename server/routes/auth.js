b32e66b2ddd9b25d3ced5299575cc8fc0baee688
Vivek Kumar <vivekkumar26oct@gmail.com>
2026-05-10 22:35:55 +0530

    Fixed model filenames

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const User = require("../models/user.js");
+const User = require("../models/User.js");
 
 const router = express.Router();
 
